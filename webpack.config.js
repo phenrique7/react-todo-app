88a7bc29ef3494ffb4261d07d48e4f750e9682e1
Paulo Henrique <pauloh1288@gmail.com>
2018-12-08 18:10:55 -0300

    feat: add css modules dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -15,6 +15,20 @@ module.exports = {
         use: {
           loader: 'babel-loader',
         }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          { loader: 'style-loader' },
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
+              camelCase: true,
+              localIdentName: '[name]__[local]--[hash:base64:5]',
+            }
+          }
+        ]
       }
     ]
   },
